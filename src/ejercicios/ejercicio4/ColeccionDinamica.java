75b3d37f2ad7711e5570a1d5e177235ded8e0b0e
ManuelMolino <mmolmil158@g.educaand.es>
2024-12-05 13:32:32 +0100

    Fin del tema

--- src/ejercicios/ejercicio4/ColeccionDinamica.java
+++ src/ejercicios/ejercicio4/ColeccionDinamica.java
@@ -1,8 +1,6 @@
 package ejercicios.ejercicio4;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ColeccionDinamica {
     public static List<String> leerCadenas() {
@@ -22,7 +20,46 @@ public class ColeccionDinamica {
 
     public static List<String> obtenerCadenasMasLargas(List<String> cadenas) {
         List<String> cadenasMasLargas = new ArrayList<>();
-        //falta el código
+        int longitudCadena = 0;
+        for (String cadena : cadenas) {
+            if (cadena.length() == longitudCadena) {
+                cadenasMasLargas.add(cadena);  //añadimos otra cadena de igual longitud
+            } else if (cadena.length() > longitudCadena) {
+                longitudCadena = cadena.length();  //nueva longitud de cadena mas grande
+                cadenasMasLargas.clear();  //vaciamos las cadenas de menor longitud
+                cadenasMasLargas.add(cadena);  //añadimos la nueva cadena de mayor longitud
+            }
+        }
         return cadenasMasLargas;
     }
+
+    public static boolean contieneCadena(List<String> cadenas, String cadena) {
+        return cadenas.contains(cadena);
+    }
+
+    public static List<String> unirListas(List<String> cadenasAuxiliares, List<String> cadenas) {
+        /*for (String cadena : cadenas)
+            cadenasAuxiliares.add(cadena);*/
+        cadenasAuxiliares.addAll(cadenas);
+        return cadenasAuxiliares;
+    }
+
+    public static Set<String> unirListasDisjunto(List<String> cadenasAuxiliares, List<String> cadenas) {
+        Set<String> conjunto = new HashSet<>();
+        /*for (String cadena : cadenasAuxiliares)
+            conjunto.add(cadena);
+        for (String cadena : cadenas)
+            conjunto.add(cadena);*/
+        conjunto.addAll(cadenasAuxiliares);
+        conjunto.addAll(cadenas);
+        return conjunto;
+    }
 }
+
+
+
+
+
+
+
+
--- src/ejercicios/ejercicio4/Test.java
+++ src/ejercicios/ejercicio4/Test.java
@@ -1,12 +1,27 @@
 package ejercicios.ejercicio4;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Test {
     public static void main(String[] args) {
         List<String> cadenas = ColeccionDinamica.leerCadenas();
         System.out.println(cadenas);
         List<String> cadenasMayorLongitud = ColeccionDinamica.obtenerCadenasMasLargas(cadenas);
+        System.out.println("Cadenas de mayor longitud:");
+        System.out.println(cadenasMayorLongitud);
+        String cadena = "hello";
+        boolean contiene = ColeccionDinamica.contieneCadena(cadenas, cadena);
+        String mensaje = contiene ? "si" : "no";
+        System.out.printf("La cadena %s %S está en la colección%n", cadena, mensaje);
+        List<String> cadenasAuxiliares = new ArrayList<>(List.of("uno", "dos", "tres", "cuatro"));
+        List<String> listaUnion = ColeccionDinamica.unirListas(cadenasAuxiliares, cadenas);
+        System.out.println("uniendo listas");
+        System.out.println(listaUnion);
+        Set<String> conjuntoUnion = ColeccionDinamica.unirListasDisjunto(cadenasAuxiliares, cadenas);
+        System.out.println("uniendo listas disjunto");
+        System.out.println(conjuntoUnion);
 
     }
 }
